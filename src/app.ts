c457b008c5f10a644729f6faf5c14e9cd3e99d9e
Chi Danh <chidanh.bp@gmail.com>
2019-05-21 23:51:32 +0700

    update

--- src/app.ts
+++ src/app.ts
@@ -7,8 +7,28 @@ export default class App {
 
     constructor(controllers: any, port: number) {
         this.app = express();
-        this.port = port;    
+        this.port = port;   
+        
+        this.initializeControllers(controllers);
+        this.initializeMiddlewares();
     }
 
-    
+    /**
+     * listen
+     */
+    public listen() {
+        this.app.listen(this.port, () => {
+            console.log(`App listening on the port ${this.port}`);
+        });
+    }
+
+    private initializeMiddlewares() {
+        this.app.use(bodyParser());
+    }
+
+    private initializeControllers(controllers: any) {
+        controllers.forEach((controller: any) => {
+            this.app.use("/", controller.router);
+        });
+    }
 }
